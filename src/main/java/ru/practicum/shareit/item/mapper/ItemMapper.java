557bfe0f21fc3bc1b226b2b8db578452a5f6f885
KlochkovDimka <KlochkovDimka97@yandex.ru>
2024-05-10 02:57:40 +0300

    Merge remote-tracking branch 'origin/add-bookings' into add-bookings
    # Conflicts:
    #	src/main/java/ru/practicum/shareit/booking/service/impl/BookingServiceImpl.java

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.mapper;
 
 import org.springframework.stereotype.Component;
-import ru.practicum.shareit.booking.dto.BookingItemDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemWithComment;
 import ru.practicum.shareit.item.model.Item;
