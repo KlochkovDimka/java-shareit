bea4d4a16fa3e2ebd34bb3f0625bafd67422dc82
KlochkovDimka <KlochkovDimka97@yandex.ru>
2024-05-26 04:29:08 +0300

    feat: Поправки LocalTest

--- src/test/java/ru/practicum/shareit/booking/service/impl/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/booking/service/impl/BookingServiceImplTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingDtoController;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.storage.BookingStorage;
+import ru.practicum.shareit.excemples.NotExistStatusName;
+import ru.practicum.shareit.excemples.NotExistUserException;
 import ru.practicum.shareit.item.comment.Comment;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
@@ -19,6 +23,7 @@ import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.storage.UserStorage;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -142,4 +147,140 @@ class BookingServiceImplTest {
         assertEquals(newBookingDto.getStatus(), Status.APPROVED);
     }
 
+    @Test
+    void getBookingsByState() {
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        assertThrows(NotExistStatusName.class,
+                () -> bookingService.getBookingsByState(1L, "ALLL", 0, 1));
+    }
+
+    @Test
+    void getBookingByOwnerAndState() {
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        assertThrows(NotExistStatusName.class,
+                () -> bookingService.getBookingByOwnerAndState(1L, "ALLL", 0, 1));
+    }
+
+    @Test
+    void getBookingsByStateCurrent() {
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        when(bookingStorage.findCurrentBookingsByUser(any(), anyLong())).thenReturn(List.of(booking));
+
+        List<BookingDto> bookingDto1 = bookingService.getBookingsByState(1L, "CURRENT", 0, 1);
+
+        assertEquals(bookingDto1.size(), 1);
+    }
+
+    @Test
+    void getBookingsByStatePast() {
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        when(bookingStorage.findBookingsByBookerIdAndEndBeforeOrderByStartAsc(anyLong(), any()))
+                .thenReturn(List.of(booking));
+
+        List<BookingDto> bookingDto1 = bookingService.getBookingsByState(1L, "PAST", 0, 1);
+
+        assertEquals(bookingDto1.size(), 1);
+    }
+
+    @Test
+    void getBookingsByStateFuture() {
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        when(bookingStorage.findBookingsByBookerIdAndStartAfterOrderByStartAsc(anyLong(), any()))
+                .thenReturn(List.of(booking));
+
+        List<BookingDto> bookingDto1 = bookingService.getBookingsByState(1L, "FUTURE", 0, 1);
+
+        assertEquals(bookingDto1.size(), 1);
+    }
+
+    @Test
+    void getBookingsByStateWaiting() {
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        when(bookingStorage.findBookingsByBookerIdAndStatusOrderByStartAsc(anyLong(), any()))
+                .thenReturn(List.of(booking));
+        ;
+
+        List<BookingDto> bookingDto1 = bookingService.getBookingsByState(1L, "WAITING", 0, 1);
+
+        assertEquals(bookingDto1.size(), 1);
+    }
+
+    @Test
+    void getBookingsByStateRejected() {
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        when(bookingStorage.findBookingsByBookerIdAndStatusOrderByStartAsc(anyLong(), any()))
+                .thenReturn(List.of(booking));
+        ;
+
+        List<BookingDto> bookingDto1 = bookingService.getBookingsByState(1L, "REJECTED", 0, 1);
+
+        assertEquals(bookingDto1.size(), 1);
+    }
+
+    @Test
+    void getBookingByOwnerAndStateCurrent() {
+        Page<Booking> bookings = new PageImpl<>(List.of(booking));
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        when(bookingStorage.findBookersByOwnerIdCurrent(anyLong(), any(), any())).thenReturn(bookings);
+
+        List<BookingDto> bookingDto1 = bookingService.getBookingByOwnerAndState(1L, "CURRENT", 0, 1);
+
+        assertEquals(bookingDto1.size(), 1);
+    }
+
+    @Test
+    void getBookingsByOwnerAndStatePast() {
+        Page<Booking> bookings = new PageImpl<>(List.of(booking));
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        when(bookingStorage.findBookersByOwnerIdPast(anyLong(), any(), any()))
+                .thenReturn(bookings);
+
+        List<BookingDto> bookingDto1 = bookingService.getBookingByOwnerAndState(1L, "PAST", 0, 1);
+
+        assertEquals(bookingDto1.size(), 1);
+    }
+
+    @Test
+    void getBookingByOwnerAndStateFuture() {
+        Page<Booking> bookings = new PageImpl<>(List.of(booking));
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        when(bookingStorage.findBookersByOwnerIdFuture(anyLong(), any(), any()))
+                .thenReturn(bookings);
+
+        List<BookingDto> bookingDto1 = bookingService.getBookingByOwnerAndState(1L, "FUTURE", 0, 1);
+
+        assertEquals(bookingDto1.size(), 1);
+    }
+
+    @Test
+    void getBookingByOwnerAndStateWaiting() {
+        Page<Booking> bookings = new PageImpl<>(List.of(booking));
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        when(bookingStorage.findBookersByOwnerIdStatus(anyLong(), any(), any()))
+                .thenReturn(bookings);
+
+        List<BookingDto> bookingDto1 = bookingService.getBookingByOwnerAndState(1L, "WAITING", 0, 1);
+
+        assertEquals(bookingDto1.size(), 1);
+    }
+
+    @Test
+    void getBookingByOwnerAndStateRejected() {
+        Page<Booking> bookings = new PageImpl<>(List.of(booking));
+        when(userStorage.findById(anyLong())).thenReturn(Optional.of(user));
+        when(bookingStorage.findBookersByOwnerIdStatus(anyLong(), any(), any()))
+                .thenReturn(bookings);
+
+        List<BookingDto> bookingDto1 = bookingService.getBookingByOwnerAndState(1L, "REJECTED", 0, 1);
+
+        assertEquals(bookingDto1.size(), 1);
+    }
+
+    @Test
+    void getBookingByOwnerAndStateNotUser() {
+        when(userStorage.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(NotExistUserException.class,
+                () -> bookingService.getBookingsByState(2L, "All", 1, 1));
+    }
 }
